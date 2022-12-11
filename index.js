de11e741e5bd2c6e38e62cc05169adff2bafd990
KassieSanisclaws <86675041+KassieSanisclaws@users.noreply.github.com>
2022-12-11 13:01:54 -0500

    first commit server set up of express node application

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+
+dotenv.config();
+
+// This creates the express app framework within the application.//
+const app = express();
+// This  
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use((req, res, next)=> {
+    res.setHeader('Access-control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', "Content-Type, Accept, X-Requested-With, Origin, Authorization");
+    next();
+});
+
+// Server response on successful connection. 
+app.get("/", (req, res)=> {
+    res.send("Sever is Ready");
+});
+
+// Error message handler.
+app.use((err, req, res, next) => {
+    res.status(500).send({message: err.message});
+});
+
+// Server port set-up.
+const port = process.env.SERVER_PORT || 54441;
+app.listen(port, () => {
+    console.log(`Server is ready at http://localhost:${port}`);
+});
+
+module.exports = app;
